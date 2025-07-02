c80efbee9072853e08909506ab8628489781beb1
Abhinay Challa <abhinayc365@gmail.com>
2025-07-02 22:20:09 +0530

    Modify service, env

--- src/app/services/api-service.ts
+++ src/app/services/api-service.ts
@@ -1,9 +1,22 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Movie } from '../models';
+import { environment as env } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getMovieDetails(
+    title: string,
+    id: string
+  ): Observable<Movie> {
+    let params = new HttpParams().set(`apikey`,env.RAWG_KEY).set(`t`,title).set('i',id);
+    return this.http.get<Movie>(`$(env.BASE_URL)`,{params: params});
+  }
+
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,5 @@
-export const environment = {};
+export const environment = {
+    production: false,
+    BASE_URL: 'https://www.omdbapi.com/',
+    RAWG_KEY: 'eb2d0733',
+};
