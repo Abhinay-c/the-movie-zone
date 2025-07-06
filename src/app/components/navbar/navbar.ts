104718972a199072e6b1e7ea1959153b983835be
Abhinay Challa <abhinayc365@gmail.com>
2025-07-06 17:52:00 +0530

    Add theme toggle

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UiSwitchModule } from 'ngx-ui-switch';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [UiSwitchModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss'
 })
-export class Navbar {
+export class Navbar implements OnInit{
+  isDark = false;
 
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  toggleTheme() {
+    if(!this.isDark){
+      if(!document.body.classList.contains('dark')) {
+        document.body.classList.add('dark');
+      }
+    } else{
+      if(document.body.classList.contains('dark')) {
+        document.body.classList.remove('dark');
+      }
+    }
+  }
 }
